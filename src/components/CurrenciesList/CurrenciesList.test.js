1774de1d6a80a24d94d7ae5879e77039157fb117
agnieszkajankowska <ajankowska0404@gmail.com>
2018-04-01 16:49:43 +0200

    CurrenciesList test added -wip

--- src/components/CurrenciesList/CurrenciesList.test.js
+++ src/components/CurrenciesList/CurrenciesList.test.js
@@ -7,7 +7,28 @@ it('renders without crashing', () => {
     shallow(<CurrenciesList currencies={["USD", "AUD"]}/>);
 });
 
-it('Shows message when there are no currencies available to follow', () => {
+it('shows message when there are no currencies available to follow', () => {
     const currenciesList = mount(<CurrenciesList currencies={[]} />);
     expect(currenciesList.text()).toContain('There are no currencies available to follow.')
 });
+
+it('shows message when there are no currencies available to follow', () => {
+    const currenciesList = mount(<CurrenciesList currencies={['USD']} />);
+    expect(currenciesList.text()).not.toContain('There are no currencies available to follow.')
+});
+
+
+describe('list of currencies', () => {
+    const currencies = ['USD', 'EUR', 'CAD'];
+    const currenciesList = mount(<CurrenciesList currencies={currencies} />);
+
+    it('shows a list of available currencies', () => {
+        expect(currenciesList.find('#currenciesList option').length).toEqual(currencies.length);
+    });
+
+    currencies.forEach(currency => {
+        it(`includes a name of ${currency} on the list`, () => {
+            expect(currenciesList.containsMatchingElement(<option>{currency}</option>)).toEqual(true);
+        })
+    })
+})
